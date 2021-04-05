c17847f42260ace0ef344cb00550194aeaec85a4
Joadson Castro <joadsonchagas@aluno.uema.br>
2021-04-05 20:46:07 -0300

    Add front-end application

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { disconnect } from 'process';
+import React from 'react';
+import './App.css';
+
+import Routes from './routes';
+import CreatePoint from './pages/CreatePoint';
+
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
